ea846f1751a3ab231485be4d6bd4cb6ef18d578a
vitoriajaco <vitoriajaco@gmail.com>
2024-07-18 08:10:56 -0300

    Condicao ternaria

--- src/condicaoTernaria/BoletimEscolar.java
+++ src/condicaoTernaria/BoletimEscolar.java
@@ -1,11 +1,24 @@
-package atribuicaoTernaria;
+package condicaoTernaria;
 
 public class BoletimEscolar {
 
     public static void main (String[] args){
 
+        int media = 7;
+        int notaBaixa = 3;
+        int notaAlta = 8;
 
-        
+        String resultado = notaBaixa >= media ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+        String resultado2 = notaAlta>= media ? "Aprovado" : "Reprovado";
+        System.out.println(resultado2);
+
+        String resultado3 = notaBaixa >= media ? "Aprovado" : notaBaixa >=5 && notaBaixa<7 ? "Recuperação" : "Reprovado direto";
+        System.out.println(resultado3);
+
+
+
     }
 
 }
