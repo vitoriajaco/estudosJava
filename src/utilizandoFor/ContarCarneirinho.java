6de8b5c078ba2c9ee4a298d859ad04b7458687f8
vitoriajaco <vitoriajaco@gmail.com>
2024-07-18 15:37:42 -0300

    Estrutura do for simples

---
+++ src/utilizandoFor/ContarCarneirinho.java
@@ -0,0 +1,11 @@
+package utilizandoFor;
+
+public class ContarCarneirinho {
+    public static void main(String[] args) {
+
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.println("Carneirinho " + carneirinhos + "..");
+        }
+        System.out.println("*** E dormiu...***");
+    }
+}
