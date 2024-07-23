2a8a4fd3752ccbed1de4d1e4e856c74a8cad5f0e
vitoriajaco <vitoriajaco@gmail.com>
2024-07-23 08:33:49 -0300

    Exemplo com do while

---
+++ src/utilizandoDoWhile/ExemploDoWhile.java
@@ -0,0 +1,31 @@
+package utilizandoDoWhile;
+
+import java.util.Random;
+
+public class ExemploDoWhile {
+
+    private static int numeroTentativas = 0;
+    public static void main(String[] args) {
+
+        System.out.println("Discando um numero...");
+        do {
+            System.out.println("Telefone tocando..");
+        }while(tocando());
+
+        if (numeroTentativas<=5)
+            System.out.println("Alô!!");
+            else
+            System.out.println("nao Atendeu");
+        }
+
+
+
+        private static boolean tocando () {
+            numeroTentativas++;
+            boolean atendeu = new Random().nextInt(5)==1;
+            if (numeroTentativas==5)
+                return false;
+            else
+                return !atendeu;
+        }
+}
