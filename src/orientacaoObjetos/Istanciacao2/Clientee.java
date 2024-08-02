5c464ca3e21c4978a92a947a7d7604a6d308e10e
vitoriajaco <vitoriajaco@gmail.com>
2024-08-02 17:53:19 -0300

    Instanciação de objeto

---
+++ src/orientacaoObjetos/Istanciacao2/Clientee.java
@@ -0,0 +1,15 @@
+package orientacaoObjetos.Istanciacao2;
+
+public class Clientee {
+
+    String nome;
+    Double limiteCredito = 100.0;
+
+    public void solicitarLimiteCredito(Double valorSolicitado){
+        limiteCredito = valorSolicitado + limiteCredito;
+    }
+
+    public void comprar (Double valor){
+        limiteCredito = limiteCredito - valor;
+    }
+}
