0409d3c276ae2c1cb52be9362062215d589b8d7a
vitoriajaco <vitoriajaco@gmail.com>
2024-08-03 11:32:04 -0300

    Instanciacao com uso de enum e for

---
+++ src/orientacaoObjetos/enums/EstadosBrasileiros.java
@@ -0,0 +1,20 @@
+package orientacaoObjetos.enums;
+
+
+public enum EstadosBrasileiros {
+
+    SAO_PAULO("SP", "São Paulo"),
+    PERNAMBUCO("PE", "Pernambuco"),
+    BAHIA("BA", "Bahia"),
+    CEARA("CE", "Ceará"),
+    ALAGOAS("AL", "Alagoas");
+
+    String sigla;
+    String estado;
+
+    EstadosBrasileiros(String sigla, String estado) {
+        this.sigla = sigla;
+        this.estado = estado;
+    }
+
+}
