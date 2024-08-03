20167c159367ab81c69d4634d5a7e08dc943212c
vitoriajaco <vitoriajaco@gmail.com>
2024-08-03 18:07:48 -0300

    Variavel para data: amanha

--- src/UsandoDatas/UsingData.java
+++ src/UsandoDatas/UsingData.java
@@ -10,6 +10,9 @@ public class UsingData {
         LocalDate dataatual = LocalDate.now();
         System.out.println(dataatual);
 
+        LocalDate amanha = dataatual.plusDays(1);
+        System.out.println(amanha);
+
         String dataBrFormatter = "03/08/2024";
 
         DateTimeFormatter dataTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
