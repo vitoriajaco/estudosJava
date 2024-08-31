455f77b2db60224ad8eaebd0d5c3c08c40146a41
vitoriajaco <vitoriajaco@gmail.com>
2024-08-31 18:48:12 -0300

    Array de uma maneira mais simples e limpa

--- src/utilizandoArrays/UtilizandoArray.java
+++ src/utilizandoArrays/UtilizandoArray.java
@@ -14,6 +14,16 @@ public class UtilizandoArray {
             System.out.println("O valor no index " + i +" é = " + valor);
         }
 
+        System.out.println("Forma mais limpa de implementar");
+
+        Integer[] arrays = new Integer[] {3,6,9};
+
+        for (int i = 0; i< arrays.length; i++){
+            Integer valor = arrays[i];
+            System.out.println("O valor no index " + i +" é = " + valor);
+        }
+
+
 
 
 
