12766f4926d4f39dca4b05875e61e5c0da13ef1f
vitoriajaco <vitoriajaco@gmail.com>
2024-08-31 18:48:55 -0300

    Big decimal

--- src/BigDecimalExample/UsingBD.java
+++ src/BigDecimalExample/UsingBD.java
@@ -11,12 +11,4 @@ public class UsingBD {
     }
 }
 
-/*
-BigDecimal.TEN é uma constante que representa o valor 10.
-O método divide é usado para dividir 10 pelo divisor (3).
-O segundo argumento (2) especifica que o resultado deve ter 2 casas decimais.
-O terceiro argumento (RoundingMode.HALF_EVEN) especifica o modo de arredondamento.
-HALF_EVEN é um modo de arredondamento
-que arredonda para o número par mais próximo quando o valor está exatamente no meio.
 
-*/
