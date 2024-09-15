e95f9edc860ef2ed642c000faee9a9fa163d5bb9
vitoriajaco <vitoriajaco@gmail.com>
2024-09-15 15:31:09 -0300

    Adicionando comentarios ao codigo

--- src/utilizandoArrays/UtilizandoArrayList.java
+++ src/utilizandoArrays/UtilizandoArrayList.java
@@ -7,12 +7,14 @@ public class UtilizandoArrayList {
 
     public static void main(String[] args) {
 
+    //Adicionando itens a uma lista
+
         List linguagens = new ArrayList<>();
         linguagens.add("Java");
         linguagens.add("Cobol");
         linguagens.add("C#");
         linguagens.add("Python");
-        linguagens.add(2, "Node.js");
+        linguagens.add(2, "Node.js"); //Modo de substituir ou acrescentar um item a lista numa posicao especifica
 
         System.out.println("A lista de linguagens " + linguagens);
         System.out.println("A lista de linguagens está vazia? " + linguagens.isEmpty());
@@ -20,7 +22,12 @@ public class UtilizandoArrayList {
         System.out.println("O elemento Java está na posição: " + linguagens.indexOf("Java"));
         System.out.println("O elemento na posição dois é " + linguagens.get(2));
 
+        //Acessando cada item da lista por for
+        for (Object linguagem : linguagens){
+            System.out.println(linguagem);
+        }
 
+        //Operando if e else com condicoes da lista
         if (linguagens.contains("Java")){
             System.out.println("Você é capaz de aprender");
 
