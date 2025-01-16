bb24a8d2f447626e23b46a331c0c88dbe2c11364
vitoriajaco <vitoriajaco@gmail.com>
2025-01-16 11:31:11 -0300

    Exercicio if com erro

--- src/a8/controleDeFluxoSimples/exercicios/Exercicio01.java
+++ src/a8/controleDeFluxoSimples/exercicios/Exercicio01.java
@@ -1,4 +1,22 @@
 package a8.controleDeFluxoSimples.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
+
+// Faça um programa que leia um número e escreva na tela se o número é menor, igual ou maior que zero.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero que você quer comparar");
+        int numero = scanner.nextInt();
+
+        if (numero > 0){
+            System.out.println("O numero inserido é maior que zero");
+            if (numero < 0){
+                System.out.println("O numero inserido é menor que zero");
+            }
+        } else
+            System.out.println("O numero é igual a zero");
+    }
 }
