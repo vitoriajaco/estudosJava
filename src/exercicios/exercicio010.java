94ee3cfdda34db4ff3e5d6d1bc17f2ad773e8605
vitoriajaco <vitoriajaco@gmail.com>
2025-01-28 11:44:42 -0300

    exercicio conversao de real para dolar

---
+++ src/exercicios/exercicio010.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio010 {
+    // Faça um programa para converter um certo valor em dólar para reais
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Double dolarHoje = 5.90;
+        System.out.println("Digite o valor que deseja converter: ");
+        Double valorConversao = scanner.nextDouble();
+        Double resultado = valorConversao / dolarHoje;
+        System.out.println("O valor em dolar é de: " + resultado);
+    }
+}
+
