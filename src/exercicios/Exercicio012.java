c91477d4e5760805554634dac705e511d0fdfd89
vitoriajaco <vitoriajaco@gmail.com>
2025-02-04 20:07:32 -0300

    Garantindo a regra

---
+++ src/exercicios/Exercicio012.java
@@ -0,0 +1,33 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio012 {
+
+   /* Escreva um programa que leia dois valores reais.
+    Ambos valores deverão ser lidos até que o usuário
+    digite um número no intervalo de 1 a 100.
+    Apresentar a soma dos dois valores lidos*/
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero: ");
+        Double numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo numero: ");
+        Double numero2 = scanner.nextDouble();
+
+        if ((numero1 >= 1 && numero1 <= 100)&& (numero2 >=1 && numero2 <=100)){
+            Double soma = (numero1 + numero2)/2;
+            System.out.println(soma);
+
+        } else {
+            System.out.println("O numero que vc digitou esta incorreto");
+        }
+
+
+
+        }
+    }
+
