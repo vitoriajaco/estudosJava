f7c6a750e7f1785ec2d98a47796aaf93b038f9b4
vitoriajaco <vitoriajaco@gmail.com>
2025-02-04 20:25:52 -0300

    Enunciados dos exercicios

---
+++ src/exercicios/Exercicio13.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Exercicio13 {
+   /* Escreva uma programa que lê o sexo de uma
+    pessoa. O sexo deverá ser com o tipo de dado
+    caractere e o programa deverá aceitar apenas os
+    valores “M” ou “F”.*/
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/exercicios/Exercicio14.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class Exercicio14 {
+
+   /* Escreva uma programa que lê o sexo de uma
+    pessoa. O sexo deverá ser com o tipo de dado
+    caractere e o programa deverá aceitar apenas os
+    valores “M” ou “F”.*/
+
+    public static void main(String[] args) {
+
+    }
+}
