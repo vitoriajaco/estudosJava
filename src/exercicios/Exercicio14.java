1b0352b993e5de13c1149bb95578cf4a08850f21
vitoriajaco <vitoriajaco@gmail.com>
2025-02-08 16:49:53 -0300

    Exercicio de lucro

--- src/exercicios/Exercicio14.java
+++ src/exercicios/Exercicio14.java
@@ -1,13 +1,31 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio14 {
 
-   /* Escreva uma programa que lê o sexo de uma
-    pessoa. O sexo deverá ser com o tipo de dado
-    caractere e o programa deverá aceitar apenas os
-    valores “M” ou “F”.*/
-
+  /* / Um comerciante comprou um produto e quer vendê-lo com lucro de 45% se o valor da
+    compra for menor que 20,00; caso contrário, o lucro será de 30%. Ler o valor do produto e
+    imprimir o valor da venda
+*/
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual o valor do produto?");
+
+        double produto = scanner.nextDouble();
+
+
+        if (produto < 20.0){
+            double lucroo = produto * 1.45;
+            System.out.println("O preço final é de: " + lucroo);
+        }else{
+            double lucroo = produto * 1.30;
+            System.out.println("O preço final é de: " + lucroo);
+        }
+
+        scanner.close();
 
     }
 }
