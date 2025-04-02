92d0430077a06ec6fdb0300286fad3955b8b28a3
vitoriajaco <vitoriajaco@gmail.com>
2025-04-02 11:45:45 -0300

    Usando o this para retornar objeto

--- src/exercicios/Pessoa.java
+++ src/exercicios/Pessoa.java
@@ -10,5 +10,19 @@ public class Pessoa {
         return this; // Retorna o próprio objeto
     }
 
+
+    @Override
+    public String toString() {
+        return "Pessoa{nome='" + nome + "', idade=" + idade + "}";
+    }
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        String s = pessoa.setNome("Adamastor Pitaco").toString();
+        System.out.println(s);
+
+        System.out.println(pessoa.setNome("Craudio").toString());
+    }
+
     
 }
