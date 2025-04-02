4509783ec5a544e94bcf211a1bbc9d13c4acd761
vitoriajaco <vitoriajaco@gmail.com>
2025-04-02 11:43:12 -0300

    Usando o this para retornar objeto

---
+++ src/exercicios/Pessoa.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class Pessoa {
+
+    String nome = "Ameinda";
+    int idade = 16;
+
+    public Pessoa setNome(String nome) {
+        this.nome = nome;
+        return this; // Retorna o próprio objeto
+    }
+
+    
+}
